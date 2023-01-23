6cc54b1531b0e2b509964d82360e86221a28916f
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-23 18:08:53 -0300

    FEAT: First week job

---
+++ src/controllers/index.ts
@@ -0,0 +1,22 @@
+export * from "./users-controller";
+export * from "./authentication-controller";
+export * from "./events-controller";
+export * from "./enrollments-controller";
+
+
+
+const races = {
+    name: "",
+    cientific: "",
+    picture: "",
+    description: "",
+    rank: {
+        position: "",
+        classe:""
+    },
+    habilities: "",
+    powers: {
+        force: "",
+        defence: ""
+    }
+}
